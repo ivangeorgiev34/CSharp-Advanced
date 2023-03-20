62bb4b6ee3cc26cd0f1f2e07d3a910faf33180c4
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:07:02 +0200

    Add exercise 04.MatchingBrackets

---
+++ Stacks and Queues - Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (expression[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string substring = expression.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
