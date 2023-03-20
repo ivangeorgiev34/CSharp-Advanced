a28e6a9f8a2555ba1c9dd9760eef574a5df4c9c0
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:06:42 +0200

    Add exercise 03.SimpleCalculator

---
+++ Stacks and Queues - Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expression = Console.ReadLine().Split();
+            Stack<string> stack = new Stack<string>();
+            int a = 0;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                stack.Push(expression[i]);
+                int result = 0;
+                if (stack.Count == 3)
+                {
+                    int firstNumber = int.Parse(stack.Pop());
+                    var operation = stack.Pop();
+                    int secondNumber = int.Parse(stack.Pop());// 2 + 5 + 10 - 2 - 1
+                    if (operation == "+")
+                    {
+                        result = firstNumber + secondNumber;
+                    }
+                    else if (operation == "-")
+                    {
+                        result = secondNumber - firstNumber;
+                    }
+                    stack.Push(result.ToString());
+                }
+
+            }
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
