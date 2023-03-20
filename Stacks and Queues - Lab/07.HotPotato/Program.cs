06f8e8ed4e37cc9f6e1fcef4de4bbf64c9a6b50e
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:07:54 +0200

    Add exercise 07.HotPotato

---
+++ Stacks and Queues - Lab/07.HotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(input);
+            int toss = 1;
+            while (queue.Count > 1)
+            {
+                string currentKid = queue.Dequeue();
+                if (toss == n)
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    toss = 1;
+                    continue;
+                }
+                queue.Enqueue(currentKid);
+                toss++;
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
