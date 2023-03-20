f20aeac97c6e8814dbd088db22bc9bbffd2e0b47
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:07:21 +0200

    Add exercise 05.PrintEvenNumbers

---
+++ Stacks and Queues - Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            string[] numbers = Console.ReadLine().Split();
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                if (int.Parse(numbers[i]) % 2 == 0)
+                {
+                    queue.Enqueue(int.Parse(numbers[i]));
+                }
+            }
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
