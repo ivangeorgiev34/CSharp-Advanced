c434a1195d947b711420f8077b1fa8ca075f87d1
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:08:13 +0200

    Add exercise 08.TrafficJam

---
+++ Stacks and Queues - Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int counter = 0;
+            while (command != "end")
+            {
+                if (command == "green" && queue.Count > 0)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            string currentCar = queue.Dequeue();
+                            Console.WriteLine($"{currentCar} passed!");
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+            return;
+        }
+    }
+}
