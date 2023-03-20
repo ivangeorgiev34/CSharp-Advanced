54567c5e7e35d4511f2b147c59b96a16741d4528
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:05:52 +0200

    Add exercise 01.ReverseAString

---
+++ Stacks and Queues - Lab/01.ReverseAString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            string input = Console.ReadLine();
+            foreach (char character in input)
+            {
+                stack.Push(character.ToString());
+            }
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
