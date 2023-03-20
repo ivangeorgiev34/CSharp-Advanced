b46beb6fac2937c9e1f6e22badb399dc5b8ee37c
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-20 12:07:37 +0200

    Add exercise 06.Supermarket

---
+++ Stacks and Queues - Lab/06.Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            string input = Console.ReadLine();
+            int counter = -1;
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                    while (input != "End")
+                    {
+                        input = Console.ReadLine();
+                        counter++;
+                    }
+                    break;
+                }
+                queue.Enqueue(input);
+                input = Console.ReadLine();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine($"{counter} people remaining.");
+            }
+            else
+            {
+                Console.WriteLine($"{queue.Count} people remaining.");
+            }
+        }
+    }
+}
