410cca21443670e67c480177cc19bf17cbcd70cc
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-22 19:52:29 +0200

    Add exercise 01.SumMatrixElements

---
+++ Multidimensional Arrays - Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arrData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arrData[col];
+                    sum = sum + matrix[row, col];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
