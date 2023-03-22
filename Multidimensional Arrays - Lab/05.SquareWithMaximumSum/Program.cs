9d30c2c1cefcaea153b5c64dad8ff2a21d7900e6
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-22 19:54:23 +0200

    Add exercise 05.SquareWithMaximumSum

---
+++ Multidimensional Arrays - Lab/05.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _05.SquareWithMaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+            int max = int.MinValue;
+            int first = 0;
+            int second = 0;
+            int third = 0;
+            int fourth = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row + 1 < rows && col + 1 < cols)
+                    {
+                        int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                        if (sum > max)
+                        {
+                            first = matrix[row, col];
+                            second = matrix[row, col + 1];
+                            third = matrix[row + 1, col];
+                            fourth = matrix[row + 1, col + 1];
+                            max = sum;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{first} {second}");
+            Console.WriteLine($"{third} {fourth}");
+            Console.WriteLine(max);
+        }
+    }
+}
