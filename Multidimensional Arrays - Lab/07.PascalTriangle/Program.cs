13066a5d6319b9d2d41f06cfba3b40809b82ac7d
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-22 19:55:15 +0200

    Add exercise 07.PascalTriangle

---
+++ Multidimensional Arrays - Lab/07.PascalTriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] jaggedArray = new long[n][];
+            for (int row = 0; row < n; row++)
+            {
+                jaggedArray[row] = new long[row + 1];
+                if (row == 0)
+                {
+                    jaggedArray[row][row] = 1;
+                    continue;
+                }
+                for (int i = 0; i <= row; i++)
+                {
+                    if (i == 0)
+                    {
+                        jaggedArray[row][i] = 1;
+                        continue;
+                    }
+                    if (i == row)
+                    {
+                        jaggedArray[row][i] = 1;
+                        continue;
+                    }
+                    jaggedArray[row][i] = jaggedArray[row - 1][i] + jaggedArray[row - 1][i - 1];
+                }
+            }
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write($"{jaggedArray[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
