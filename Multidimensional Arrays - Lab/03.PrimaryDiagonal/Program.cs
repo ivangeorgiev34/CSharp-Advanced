736352700355430931efcc66dc345db65471d31f
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-22 19:53:38 +0200

    Add exercise 03.PrimaryDiagonal

---
+++ Multidimensional Arrays - Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum + matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
