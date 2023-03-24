8901e9afeeca39ac42fc092fce0bfc6ee165e799
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:38:07 +0200

    Add exercise 03.LargestThreeNumbers

---
+++ Sets and Dictionaries Advanced - Lab/03.LargestThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.LargestThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            input = input.OrderByDescending(n => n).ToArray();
+            if (input.Length < 3)
+            {
+                Console.WriteLine(string.Join(" ", input));
+                return;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
