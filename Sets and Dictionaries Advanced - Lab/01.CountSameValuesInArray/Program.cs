671b2e7ab4350a5566c8a2d74dcf1196491f8859
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:37:03 +0200

    Add exercise 01.CountSameValuesInArray

---
+++ Sets and Dictionaries Advanced - Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(x => double.Parse(x)).ToArray();
+            Dictionary<double, int> dictionary = new Dictionary<double, int>();
+            foreach (double item in input)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    dictionary.Add(item, 0);
+                }
+
+                dictionary[item] = dictionary[item] + 1;
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
