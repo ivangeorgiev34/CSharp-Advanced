78f4ca2618b5e5e5680091599bbb12f2d13cbfe2
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:39:35 +0200

    Add exercise 07.ParkingLot

---
+++ Sets and Dictionaries Advanced - Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> numbers = new HashSet<string>();
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                var tokens = Regex.Split(input, ", ");
+
+                if (tokens[0] == "IN")
+                {
+                    numbers.Add(tokens[1]);
+                }
+                else if (tokens[0] == "OUT")
+                {
+                    numbers.Remove(tokens[1]);
+                }
+                input = Console.ReadLine();
+            }
+            if (numbers.Count > 0)
+            {
+                foreach (var item in numbers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Parking Lot is Empty");
+            }
+        }
+    }
+}
