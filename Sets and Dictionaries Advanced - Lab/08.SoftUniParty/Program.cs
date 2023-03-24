55371429fdcce57d10fe943e5d101be606d65217
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:39:57 +0200

    Add exercise 08.SoftUniParty

---
+++ Sets and Dictionaries Advanced - Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+            }
+            while ((input = Console.ReadLine()) != "END")
+            {
+                vipGuests.Remove(input);
+                regularGuests.Remove(input);
+            }
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            foreach (var item in vipGuests)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in regularGuests)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
