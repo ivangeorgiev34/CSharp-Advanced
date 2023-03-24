5f1d67e925bda82bf8c348c2eaa84a7e75708a47
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:37:45 +0200

    Add exercise 02.AverageStudentGrades

---
+++ Sets and Dictionaries Advanced - Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
