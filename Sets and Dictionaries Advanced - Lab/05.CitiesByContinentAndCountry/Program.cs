2bfee93b520117ad60d443111606c0946dd7a000
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-24 08:38:51 +0200

    Add exercise 05.CitiesByContinentAndCountry

---
+++ Sets and Dictionaries Advanced - Lab/05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> dictionary = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                string continent = tokens[0];
+                string country = tokens[1];
+                string town = tokens[2];
+                if (!dictionary.ContainsKey(continent))
+                {
+                    dictionary.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!dictionary[continent].ContainsKey(country))
+                {
+                    dictionary[continent].Add(country, new List<string>());
+                }
+                dictionary[continent][country].Add(town);
+            }
+            foreach (var continent in dictionary)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var item in continent.Value)
+                {
+                    Console.WriteLine($"  {item.Key} -> {string.Join(", ", item.Value)}");
+                }
+            }
+        }
+    }
+}
