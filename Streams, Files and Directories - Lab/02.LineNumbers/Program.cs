9a3c429855253f7533325e41cfff3999e8c8e5b7
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-26 21:36:53 +0300

    Add exercise 02.LineNumbers

---
+++ Streams, Files and Directories - Lab/02.LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            int row = 1;
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string currentLine = reader.ReadLine();
+                        writer.WriteLine($"{row}. {currentLine}");
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
+
