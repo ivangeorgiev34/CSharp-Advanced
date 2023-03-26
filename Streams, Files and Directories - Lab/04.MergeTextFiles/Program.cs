c1448a63351c0df2a432581cbf18f5095a6d045e
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-26 21:37:46 +0300

    Add exercise 04.MergeTextFiles

---
+++ Streams, Files and Directories - Lab/04.MergeTextFiles/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace _04.MergeTextFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using (StreamReader firstReader = new StreamReader(firstInputFilePath))
+            {
+                using (StreamReader secondReader = new StreamReader(secondInputFilePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+
+                        while (!firstReader.EndOfStream || !secondReader.EndOfStream)
+                        {
+                            if (firstReader.EndOfStream)
+                            {
+                                while (!secondReader.EndOfStream)
+                                {
+                                    writer.WriteLine(secondReader.ReadLine());
+                                }
+                                break;
+                            }
+                            if (secondReader.EndOfStream)
+                            {
+                                while (!firstReader.EndOfStream)
+                                {
+                                    writer.WriteLine(firstReader.ReadLine());
+                                }
+                                break;
+                            }
+                            string currentFirstLine = firstReader.ReadLine();
+                            string currentSecondLine = secondReader.ReadLine();
+                            writer.WriteLine(currentFirstLine);
+                            writer.WriteLine(currentSecondLine);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
