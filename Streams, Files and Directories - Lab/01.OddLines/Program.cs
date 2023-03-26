1e7a6e8bf540c5a941994ae13b0f8d3fe866ac09
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-26 21:36:24 +0300

    Add exercise 01.OddLines

---
+++ Streams, Files and Directories - Lab/01.OddLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace _02.OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            int row = 1;
+            using (reader)
+            {
+                using (writer)
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string currentLine = reader.ReadLine();
+                        if (row % 2 != 0)
+                        {
+                            writer.WriteLine(currentLine);
+                        }
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
+
