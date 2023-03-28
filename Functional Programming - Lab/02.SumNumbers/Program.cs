af262097a7dad9d375969017684df0d014865e12
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-28 18:22:09 +0300

    Add exercise 02.SumNumbers

---
+++ Functional Programming - Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).Count());
+            Console.WriteLine(input.Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).Sum());
+        }
+    }
+}
