d6f2d25cd2825a2d1945540e06b8551e2b2f1953
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-28 18:22:31 +0300

    Add exercise 03.CountUppercaseWords

---
+++ Functional Programming - Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = x => x[0].ToString() == x[0].ToString().ToUpper();
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(x=>x).Where(func)));
+        }
+    }
+}
