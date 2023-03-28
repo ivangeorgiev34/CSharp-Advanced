7da0d74afdc1aa4f116761931c65de7485408992
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-28 18:21:41 +0300

    Add exercise 01.SortEvenNumbers

---
+++ Functional Programming - Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+            Console.WriteLine(string.Join(", ",Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Where(x => x % 2 == 0).OrderBy(x => x).ToArray())) ;
+        }
+    }
+}
