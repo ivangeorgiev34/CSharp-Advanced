48cfb3e5b93feef3ab0983d8157f7c538e891e8c
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-03-28 18:23:29 +0300

    Add exercise 04.AddVAT

---
+++ Functional Programming - Lab/04.AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, string> function = n => (n * 1.2D).ToString("f2");
+            //1.38, 2.56, 4.4
+            Console.WriteLine(string.Join(Environment.NewLine, (Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(n => double.Parse(n)).Select(function).ToArray())));
+        }
+
+    }
+
+}
