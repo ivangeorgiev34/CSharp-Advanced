4b8efe304f5b2a696e407b96ddf8091b2c3b2370
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-02 16:15:08 +0300

    Add exercise 07.PredicateForNames

---
+++ Functional Programming - Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> list = new List<string>(names);
+            Action<List<string>, int> print = (list, n) =>
+             {
+                 for (int i = 0; i < list.Count; i++)
+                 {
+                     if (list[i].Length <= n)
+                     {
+                         Console.WriteLine(list[i]);
+                     }
+                 }
+             };
+            print(list,n);
+        }
+    }
+}
