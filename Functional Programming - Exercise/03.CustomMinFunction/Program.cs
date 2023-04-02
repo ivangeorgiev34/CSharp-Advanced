01684d970ff29bb3f2738cbb829570f65c6a2140
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-02 16:13:26 +0300

    Add exercise 03.CustomMinFunction

---
+++ Functional Programming - Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            Func<int[], int> minNumber = numbers =>
+             {
+                 int min = int.MaxValue;
+                 for (int i = 0; i < numbers.Length; i++)
+                 {
+                     if (numbers[i] < min)
+                     {
+                         min = numbers[i];
+                     }
+                 }
+                 return min;
+             };
+            Console.WriteLine(minNumber(numbers));
+        }
+    }
+}
