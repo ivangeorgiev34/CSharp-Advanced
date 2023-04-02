8515f0f1703f4a365e1ca34649c801b71eb60c3b
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-02 16:16:52 +0300

    Add exercise 011.TriFunction

---
+++ Functional Programming - Exercise/011.TriFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _011.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> list = new List<string>(names);
+            Func<string, int, bool> checkValidUsername = (name, num) =>
+              {
+                  int sum = 0;
+                  for (int i = 0; i < name.Length; i++)
+                  {
+                      sum = sum + (int)name[i];
+                  }
+                  if (sum >= num)
+                  {
+                      return true;
+                  }
+                  return false;                 
+              };
+            Func<Func<string, int,bool>, List<string>, int, string> firstOrDefault = (f, list, s) =>  list.FirstOrDefault(name => f(name, s));
+            Console.WriteLine(firstOrDefault(checkValidUsername,list,n));
+        }
+    }
+}
