097653758aaf1b939ea4942a6b31207e945a7395
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-02 16:13:07 +0300

    Add exercise 02.KnightsOfHonor

---
+++ Functional Programming - Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = (k) =>
+            {
+                foreach (var item in k)
+                {
+                    Console.WriteLine($"Sir {item}");
+                }
+            };
+            string[] knights = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            print(knights);
+        }
+    }
+}
