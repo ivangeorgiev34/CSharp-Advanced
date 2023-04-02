508b90acaa62173428a754d7eed0457e74e3a929
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-02 16:11:51 +0300

    Add exercise 01.ActionPrint

---
+++ Functional Programming - Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = strings => Console.WriteLine(string.Join(Environment.NewLine,strings));
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            print(names);
+        }
+    }
+}
