e7a33ea1db96958e3c94150454a1ae2f1d0233ed
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-05 20:32:09 +0300

    Add Defining Classes - Lab exercises

---
+++ Defining Classes - Lab/Car.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tire) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tire = tire;
+        }
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+        private Engine engine;
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+        private Tire[] tire;
+
+        public Tire[] Tire
+        {
+            get { return tire; }
+            set { tire = value; }
+        }
+
+
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (FuelConsumption / 100) * distance >= 0)
+            {
+                FuelQuantity = FuelQuantity - (FuelConsumption / 100) * distance;
+                return;
+            }
+            Console.WriteLine($"Not enough fuel to perform the trip!");
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: { this.Model}\nYear: { this.Year}\nFuel: { this.FuelQuantity:F2}";
+
+        }
+    }
+}
---
+++ Defining Classes - Lab/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] command = Console.ReadLine().Split();
+            List<Tire[]> tireList = new List<Tire[]>();
+            while ($"{command[0]} {command[1]} {command[2]}" != "No more tires")
+            {
+                tireList.Add(new Tire[] { new Tire(int.Parse(command[0]), double.Parse(command[1])), new Tire(int.Parse(command[2]), double.Parse(command[3])), new Tire(int.Parse(command[4]), double.Parse(command[5])), new Tire(int.Parse(command[6]), double.Parse(command[7])) });
+                command = Console.ReadLine().Split();
+            }
+            command = Console.ReadLine().Split();
+            List<Engine> engineList = new List<Engine>();
+            while ($"{command[0]} {command[1]}" != "Engines done")
+            {
+                engineList.Add(new Engine(int.Parse(command[0]), double.Parse(command[1])));
+                command = Console.ReadLine().Split();
+            }
+            command = Console.ReadLine().Split();
+            List<Car> carsList = new List<Car>();
+            while ($"{command[0]} {command[1]}" != "Show special")
+            {
+                carsList.Add(new Car(command[0], command[1], int.Parse(command[2]), double.Parse(command[3]), double.Parse(command[4]), engineList[int.Parse(command[5])], tireList[int.Parse(command[6])]));
+                command = Console.ReadLine().Split();
+            }
+            for (int i = 0; i < carsList.Count; i++)
+            {
+                if (carsList[i].Year >= 2017 && carsList[i].Engine.HorsePower > 330)
+                {
+                    double pressureSum = 0;
+                    for (int j = 0; j < carsList[i].Tire.Length; j++)
+                    {
+                        pressureSum = carsList[i].Tire[j].Pressure + pressureSum;
+                    }
+                    if (pressureSum > 9 && pressureSum < 10)
+                    {
+                        carsList[i].Drive(20);
+                        Console.WriteLine($"Make: {carsList[i].Make}\nModel: {carsList[i].Model}\nYear: {carsList[i].Year}\nHorsePowers: {carsList[i].Engine.HorsePower}\nFuelQuantity: {carsList[i].FuelQuantity}");
+                    }
+                    
+                }
+               
+
+            }
+        }
+    }
+}
