2078161202ea76d3d369a9c0c683559bf3b71fee
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-13 16:09:45 +0300

    Add exercise 06.SpeedRacing

---
+++ Defining Classes - Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tires[] Tires { get; set; }
+        public Car(string model, int speed, int power, int weight, string type,double tire1Pressure, int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure, int tire3Age, double tire4Pressure, int tire4Age)
+        {
+            Model = model;
+            Engine = new Engine { Speed = speed, Power = power };
+            Cargo = new Cargo { Type = type, Weight = weight };
+            Tires = new Tires[4];
+            Tires[0] = new Tires { Age = tire1Age, Pressure = tire1Pressure };
+            Tires[1] = new Tires { Age = tire2Age, Pressure = tire2Pressure };
+            Tires[2] = new Tires { Age = tire3Age, Pressure = tire3Pressure };
+            Tires[3] = new Tires { Age = tire4Age, Pressure = tire4Pressure };
+        }
+    }
+}
---
+++ Defining Classes - Exercise/06.SpeedRacing/Tires.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Tires
+    {
+        public int Age { get; set; }
+        public double Pressure { get; set; }
+    }
+}
