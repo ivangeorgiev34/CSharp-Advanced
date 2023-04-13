0ecfeaf6da4ad7709f1fc49bcf4a94410820a4b7
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-13 16:10:54 +0300

    Add exercise 09.PokemonTrainer

---
+++ Defining Classes - Exercise/09.PokemonTrainer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Trainer> trainers = new List<Trainer>();
+            while (command != "Tournament")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = tokens[0];
+                string pokemonName = tokens[1];
+                string pokemonElement = tokens[2];
+                int pokemonHealth = int.Parse(tokens[3]);
+                bool flag = true;
+                for (int i = 0; i < trainers.Count; i++) // peter george peter
+                {
+                    if (trainers[i].Name == trainerName)
+                    {
+                        flag = false;
+                        Pokemon pokemon = new Pokemon() { PokemonName = pokemonName, Element = pokemonElement, Health = pokemonHealth };
+                        trainers[i].CollectionOfPokemons.Add(pokemon);
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    Trainer trainer = new Trainer(trainerName, pokemonName, pokemonElement, pokemonHealth);
+                    trainers.Add(trainer);
+                }
+                command = Console.ReadLine();
+            }
+            string element = Console.ReadLine();
+            while (element != "End")
+            {
+                foreach (Trainer trainer in trainers)
+                {
+                    bool hasPokemonOfTheElement = true;
+                    foreach (var pokemon in trainer.CollectionOfPokemons)
+                    {
+                        if (pokemon.Element == element)
+                        {
+                            trainer.Badges++;
+                            hasPokemonOfTheElement = false;
+                            break;
+                        }
+                    }
+                    if (hasPokemonOfTheElement)
+                    {
+                        for (int i = 0; i < trainer.CollectionOfPokemons.Count; i++)
+                        {
+                            trainer.CollectionOfPokemons[i].Health = trainer.CollectionOfPokemons[i].Health - 10;
+                            if (trainer.CollectionOfPokemons[i].Health <= 0)
+                            {
+                                trainer.CollectionOfPokemons.Remove(trainer.CollectionOfPokemons[i]);
+                                i++;
+                            }
+                        }
+                    }
+                }
+                element = Console.ReadLine();
+            }
+
+             // peter george josh
+            foreach (Trainer trainer in trainers.OrderByDescending(x => x.Badges))
+            {
+                Console.WriteLine($"{ trainer.Name} {trainer.Badges} {trainer.CollectionOfPokemons.Count}");
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public Trainer(string pokemonName, string pokemonElement, int pokemonHealth)
+        {
+            CollectionOfPokemons.Add(new Pokemon { PokemonName = pokemonName, Element = pokemonElement, Health = pokemonHealth });
+        }
+        public Trainer(string trainerName, string pokemonName, string pokemonElement, int pokemonHealth)
+        {
+            Name = trainerName;
+            if (CollectionOfPokemons == null)
+            {
+                CollectionOfPokemons = new List<Pokemon>();
+               
+            }
+            CollectionOfPokemons.Add(new Pokemon { PokemonName = pokemonName, Element = pokemonElement, Health = pokemonHealth });
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> CollectionOfPokemons { get; set ; }
+
+    }
+}
