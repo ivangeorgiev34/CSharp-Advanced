9739e415b8811b12d84eb12a16ce59b1a4726bcb
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-15 20:04:03 +0300

    Add exercise 03.GenericScale

---
+++ Generics - Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public bool AreEqual()
+        {
+            if (Left.CompareTo(Right) == 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
