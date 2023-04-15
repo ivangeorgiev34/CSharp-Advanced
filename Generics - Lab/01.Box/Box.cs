799ffd871e6a51d9a9f20eb1a5faf0f0f1bc36c5
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-15 20:03:14 +0300

    Add exercise 01.Box

---
+++ Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        public Box()
+        {
+            List = new List<T>();
+        }
+        public List<T> List { get; set; }
+        public int Count { get { return List.Count; } }
+        public void Add(T element)
+        {
+            List.Add(element);
+        }
+        public T Remove()
+        {
+            T elementToRemove = List[List.Count-1];
+            List.Remove(elementToRemove);
+            return elementToRemove;
+        }
+
+    }
+}
